9c67e4eebd7afe25bf7fe0deabe86c6e27bdcee8
donhcan <donhcan@outlook.com>
2023-04-06 13:50:14 -0400

    Authenticate Payment.

--- gateways/mycred-midtrans.php
+++ gateways/mycred-midtrans.php
@@ -33,12 +33,42 @@ if (!class_exists('myCred_Midtrans')):
         }
 
         public function returning() {
-         if(isset($_REQUEST['status_code'])&&$_REQUEST['status_code']==200){
-            $pending_post_id = $_REQUEST['order_id'];
-            $pending_payment = $this->get_pending_payment($pending_post_id );
-            $this->complete_payment($pending_payment,$pending_post_id);
-            $this->trash_pending_payment( $pending_post_id);
-         }
+            if(isset($_REQUEST['status_code'])&&$_REQUEST['status_code']==200){
+                $host = 'api.midtrans.com';
+                if($this->sandbox_mode)
+                    $host = 'api.sandbox.midtrans.com';
+    
+                try {
+                    $order_id = $_REQUEST['order_id'];
+                    $retrieve_status = wp_remote_get( 'https://'.$host.'/v2/'.$order_id.'/status', 
+                        array(
+                            'headers'     => 
+                            array(
+                                'Accept' => 'application/json',
+                                'Content-Type' => 'application/json',
+                                'Authorization' => 'Basic ' . base64_encode($this->prefs['server_key'] . ':')
+                            ),
+                        ) 
+                     );
+                    
+                     $status =  json_decode(wp_remote_retrieve_body( $retrieve_status ))->status_code;
+    
+                } catch ( \Exception $e ) {
+    
+                    $new_call[] = $e->getMessage();
+    
+                }
+    
+                if ( empty( $new_call ) && $status == '200' ) {
+                    $pending_post_id = $_REQUEST['order_id'];
+                    $pending_payment = $this->get_pending_payment($pending_post_id );
+                    $this->complete_payment($pending_payment,$pending_post_id);
+                    $this->trash_pending_payment( $pending_post_id);
+                }
+    
+            }
+            
+        
         }
         
         public function prep_sale($new_transaction = false) {
